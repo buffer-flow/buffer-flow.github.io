7277301c0d7c121284321c5de39ebfb1d259f858
Koen du Buf <koen.du.buf@gmail.com>
2026-05-11 11:37:13 +0200

    Use hash router to work with gh pages hopefully

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { LinkemPage } from './pages/LinkemPage'
 import { ShowemPage } from './pages/ShowemPage'
@@ -7,14 +7,14 @@ import './App.css'
 
 function App() {
   return (
-    <Router basename="/">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/linkem" element={<LinkemPage />} />
         <Route path="/showem" element={<ShowemPage />} />
         <Route path="/msg" element={<MsgPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   )
 }
 
